a65c69109b1211aedbbbeeec1dd94d661dc6648f
Natania007 <nat07@outlook.co.nz>
2024-08-22 11:56:27 +1200

    Add files via upload

---
+++ pizza.js
@@ -0,0 +1,11 @@
+console.log("I love pizza!");
+let pizzaName = "Spicy Chicken"
+let price = 14.50
+console.log("New pizza order:" + pizzaName + ". The price of the pizza is: $" + price);
+let quantity = 3;
+let familyPizza = price * 2; 
+let regularPizza = 14.50;
+let totalPrice;
+totalPrice = quantity * price;
+console.log("New pizza order: " + quantity + familyPizza + pizzaName + ". The total is 72.5$ ")
+
